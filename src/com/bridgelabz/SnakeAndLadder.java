9696c727a0a918b0397765cc42c7c443cdffb18d
Mahesh <mahesh51138@gmail.com>
2022-01-21 21:23:20 +0530

    added:WinnigPosition

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -5,27 +5,29 @@ public class SnakeAndLadder {
     public static int position=0;
     public static void main(String[] args) {
         System.out.println("Game started with single player at position " + START_POSITION);
-        Random random = new Random();
-        int dice = 1 + random.nextInt(6);
-        int options = random.nextInt(3);
-        switch (options){
-            case 0:{
-                position = position;
-                System.out.println("No Play and position : " + position);
-                break;
+        while (position < 100) {
+            Random random = new Random();
+            int dice = 1 + random.nextInt(6);
+            int options = random.nextInt(3);
+            switch (options) {
+                case 0: {
+                    position = position;  //no play
+                    break;
+                }
+                case 1: {
+                    position = position + dice;   //ladder
+                    break;
+                }
+                default: {
+                    position = position - dice;    //snake
+                    if (position < 0)
+                        position=0;
+                    break;
+                }
             }
-            case 1:{
-                position = position + dice;
-                System.out.println("Ladder and position : " + position);
-                break;
-            }
-            default:{
-                position = position - dice;
-                System.out.println("Snake and position : " + position);
-                break;
-            }
-        }
 
+        }
+        System.out.println(position);
     }
 
 
