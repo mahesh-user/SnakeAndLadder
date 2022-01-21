8c36ded90fa4b739f5e95d1c4b199528c53c220d
Mahesh <mahesh51138@gmail.com>
2022-01-21 21:30:01 +0530

    added:Count Of Dice

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,36 +2,38 @@ package com.bridgelabz;
 import java.util.Random;
 public class SnakeAndLadder {
     public static final int START_POSITION=0;
-    public static int position=0;
+    public static int POSITION =0;
+    public static int DICE_COUNT=0;
     public static void main(String[] args) {
         System.out.println("Game started with single player at position " + START_POSITION);
-        while (position < 100) {
+        while (POSITION < 100) {
             Random random = new Random();
             int dice = 1 + random.nextInt(6);
             int options = random.nextInt(3);
             switch (options) {
                 case 0: {
-                    position = position;  //no play
+                    POSITION = POSITION;  //no play
                     break;
                 }
                 case 1: {
-                    position = position + dice;   //ladder
-                    if (position > 100)
-                        position = position - dice;
-                    if (position == 100)
+                    POSITION = POSITION + dice;   //ladder
+                    if (POSITION > 100)
+                        POSITION = POSITION - dice;
+                    if (POSITION == 100)
                         System.out.println("Player Wins");
                     break;
                 }
                 default: {
-                    position = position - dice;    //snake
-                    if (position < 0)
-                        position=0;
+                    POSITION = POSITION - dice;    //snake
+                    if (POSITION < 0)
+                        POSITION =0;
                     break;
                 }
             }
-
+            DICE_COUNT = DICE_COUNT + 1;
         }
-        System.out.println(position);
+        System.out.println("Number of dice roll : " + DICE_COUNT);
+        System.out.println(POSITION);
     }
 
 
